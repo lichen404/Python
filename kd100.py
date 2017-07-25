1ff0ec66253448c1e226e200194229ef39cc676d
lichen404 <f360family@gmail.com>
2017-07-25 13:27:20 +0800

    Python
    个人的Python代码

---
+++ kd100.py
@@ -0,0 +1,68 @@
+import urllib.request
+import argparse
+import json
+guess='https://www.kuaidi100.com/autonumber/autoComNum?text={0}'
+query='https://www.kuaidi100.com/query?type={0}&postid={1}'
+def kd100_query(code,company=None):
+    company_possible=[ ]
+    if company==None:
+        f=urllib.request.urlopen(guess.format(code))
+        content=json.loads(f.read().decode('utf-8'))
+        try:
+            for  x in content['auto']:
+                company_possible.append(x['comCode'])
+            print('possible company:'+str(company_possible))
+        except:
+            print('please check your  input')
+        for p in company_possible:
+            f=urllib.request.urlopen(query.format(p,code))
+            content=json.loads(f.read().decode('utf-8'))
+            try:
+                for x in content['data']:
+                    time=x['time']
+                    context=x['context']
+                    location=x['location']
+                    print(time,context)
+            except:
+                pass
+        
+    else:
+        company=str(company)
+        f=urllib.request.urlopen(query.format(company,code))
+        content=json.loads(f.read().decode('utf-8'))
+        try:
+            for x in content['data']:
+                time=x['time']
+                context=x['context']
+                location=x['location']
+                print(time,context)
+            if int(content['status']) != 200:
+                print('the company\'s name  may be wrong')
+        except:
+            print('please check your input')
+if __name__=='__main__':
+    parser=argparse .ArgumentParser(description='query express info use kuaidi100 api')
+    parser.add_argument('-c','--code',type=str,help='express code')
+    parser.add_argument('-p', '--company', type=str,
+                        help='express company, will auto '
+                             'guess company if not provided',
+                        default=None)
+   
+    args = parser.parse_args()
+    code=args.code
+    company=args.company
+ 
+    kd100_query(code,company=company)
+            
+         
+        
+        
+        
+        
+            
+            
+       
+    
+
+
+        
---
+++ zipHack.py
@@ -0,0 +1,28 @@
+import zipfile
+import argparse
+from threading import Thread
+def extractFile(zFile,password):
+    try:
+        zFile.extractall(pwd=password)
+        print ('[+] Password = ' + password.decode('utf-8') + '\n')
+        exit(0)
+    except Exception as e:
+        pass
+        
+
+def main(dictionary,Zip):
+    zFile = zipfile.ZipFile(Zip)
+    passFile = open(dictionary)
+    for line in passFile.readlines():
+        password = line.strip('\n').encode()
+        t = Thread(target=extractFile, args=(zFile,password))
+        t.start()
+        
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser(usage='python demo.py  -d 字典名 -z 文件名')
+    parser.add_argument("-d",type=str)
+    parser.add_argument("-z",type=str)
+    args = parser.parse_args()
+    dictionary=args.d
+    Zip=args.z
+    main(dictionary,Zip)
